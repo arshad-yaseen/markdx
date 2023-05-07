1b16f8fa15aa3b6331d546864cc8aa579e8f841c
Arshad Yaseen <arshadpyaseen@gmail.com>
2023-05-07 14:22:31 +0530

    initial frontend setups completed

---
+++ config/site.ts
@@ -0,0 +1,13 @@
+import { SiteConfig } from "types";
+
+export const siteConfig: SiteConfig = {
+  name: "MarkDX - AI Markdown Editor",
+  description:
+    "MarkDX is an open source AI markdown editor, which can help you write markdown documents more efficiently.",
+  url: "https://markdx.site",
+  ogImage: "https://markdx.site/og.jpg",
+  links: {
+    twitter: "https://twitter.com/arshadyaseeen",
+    github: "https://github.com/arshad-yaseen/markdx",
+  },
+};
---
+++ types/index.d.ts
@@ -0,0 +1,10 @@
+export type SiteConfig = {
+  name: string;
+  description: string;
+  url: string;
+  ogImage: string;
+  links: {
+    twitter: string;
+    github: string;
+  };
+};
