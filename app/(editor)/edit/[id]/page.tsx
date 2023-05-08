b14c92364972c9795fa17262ee6a18e6f80bd9aa
Arshad Yaseen <arshadpyaseen@gmail.com>
2023-05-08 10:46:56 +0530

    editor initialized

--- app/(editor)/edit/[id]/page.tsx
+++ app/(editor)/edit/[id]/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function page({ params }: { params: { id: string } }) {
   return (
     <div>
-      <h1>Page: {params.id}</h1>
+     
     </div>
   );
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -5,12 +5,20 @@ import Link from "next/link";
 import React from "react";
 import { Button } from "./ui/button";
 import { useTheme } from "next-themes";
+import { usePathname } from "next/navigation";
 
 function Header() {
   const { theme } = useTheme();
+  const pathname = usePathname();
+  const isEditor =
+    pathname.includes("/dashboard") || pathname.includes("/edit");
 
   return (
-    <header className="w-full py-6 h-20 flex lg:px-12 px-6">
+    <header
+      className={`w-full ${
+        isEditor ? "border-b h-14" : "h-20"
+      } py-6  flex lg:px-12 px-6`}
+    >
       <div className="flex h-full w-1/2 items-center">
         <Link href="/" className="flex items-center">
           <Image
@@ -25,7 +33,9 @@ function Header() {
             width={"0"}
             height={"0"}
           />
-          <h4 className="font-bold font-heading text-xl ml-2">MarkDX</h4>
+          <h4 className="font-bold font-heading text-xl ml-2">
+            {isEditor ? "Editor" : "MarkDX"}
+          </h4>
         </Link>
       </div>
       <div className="flex h-full w-1/2 items-center justify-end">
