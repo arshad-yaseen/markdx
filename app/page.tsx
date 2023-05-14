2eb7ed21f85217e3e5c191775e4a4c624d1b4962
arshad-yaseen <arshadpyaseen@gmail.com>
2023-05-14 16:46:48 +0530

    solved filename error

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import { Toaster } from "sonner"
 
-import Features from "../components/features"
-import Hero from "../components/hero"
+import Features from "@/components/features"
+import Hero from "@/components/hero"
 import OpenSource from "@/components/open-source"
 import SiteFooter from "@/components/site-footer"
 
