6ea9a0572525ba833e67255bf5b934061ed46717
arshad-yaseen <arshadpyaseen@gmail.com>
2023-11-01 22:12:48 +0530

    chore: home gradiwnt

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -6,6 +6,7 @@ import SiteHero from "@/components/site-hero"
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center">
+      <div className="jumbo absolute -inset-[10px] opacity-[12%]"></div>
       <SiteHero />
       <SiteFeatures />
       <OpenSource />
