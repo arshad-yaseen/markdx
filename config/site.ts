7764ce06bb0004ce00156c5e26d16c9a73cf02bb
arshad-yaseen <arshadpyaseen@gmail.com>
2023-11-22 20:09:55 +0530

    test: test

--- config/site.ts
+++ config/site.ts
@@ -16,7 +16,9 @@ export const siteConfig = {
     twitter: "https://twitter.com/arshadyaseeen",
     twitterId: "@arshadyaseeen",
     github: "https://github.com/arshad-yaseen",
-    website: "https://arshadyaseen.com",
-    mail: "m@arshadpyaseen.com",
+    website: "https://arshadd.site",
+    mail: "arshadpyaseen@gmail.com",
   },
 }
+
+export const githubRepo: string = "https://github.com/arshad-yaseen/markdx"
